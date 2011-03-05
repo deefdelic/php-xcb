c1e32dbc3cb9019c2f30bb657b2ec6347418ff8a
dthompso99@gmail.com <dthompso99@gmail.com@754293b8-5aeb-2763-0a8f-6568e7708fb1>
2011-03-05 00:25:15 +0000

    Initial Commit

---
+++ trunk/php_xcb.h
@@ -0,0 +1,115 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2010 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id: header 297205 2010-03-30 21:09:07Z johannes $ */
+
+#ifndef PHP_XCB_H
+#define PHP_XCB_H
+
+extern zend_module_entry xcb_module_entry;
+#define phpext_xcb_ptr &xcb_module_entry
+
+#ifdef PHP_WIN32
+#	define PHP_XCB_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#	define PHP_XCB_API __attribute__ ((visibility("default")))
+#else
+#	define PHP_XCB_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+PHP_MINIT_FUNCTION(xcb);
+PHP_MSHUTDOWN_FUNCTION(xcb);
+PHP_RINIT_FUNCTION(xcb);
+PHP_RSHUTDOWN_FUNCTION(xcb);
+PHP_MINFO_FUNCTION(xcb);
+
+PHP_FUNCTION(xcb_init);
+PHP_FUNCTION(xcb_root_id);
+PHP_FUNCTION(xcb_root_width_in_pixels);
+PHP_FUNCTION(xcb_root_height_in_pixels);
+PHP_FUNCTION(xcb_generate_id);
+PHP_FUNCTION(xcb_create_window);
+PHP_FUNCTION(xcb_map_window);
+PHP_FUNCTION(xcb_flush);
+PHP_FUNCTION(xcb_unmap_window);
+PHP_FUNCTION(xcb_configure_window);
+PHP_FUNCTION(xcb_configure_window_border);
+PHP_FUNCTION(xcb_configure_window_size);
+PHP_FUNCTION(xcb_configure_window_pos);
+PHP_FUNCTION(xcb_configure_window_lower);
+PHP_FUNCTION(xcb_configure_window_raise);
+PHP_FUNCTION(xcb_configure_window_events);
+PHP_FUNCTION(xcb_configure_window_events_root);
+PHP_FUNCTION(xcb_reparent_window);
+PHP_FUNCTION(xcb_get_geometry);
+PHP_FUNCTION(xcb_query_tree);
+PHP_FUNCTION(xcb_wait_for_event);
+PHP_FUNCTION(xcb_destroy_window);
+PHP_FUNCTION(xcb_list_extensions);
+PHP_FUNCTION(xcb_query_extension);
+PHP_FUNCTION(xcb_get_default_colormap);
+PHP_FUNCTION(xcb_create_colormap);
+PHP_FUNCTION(xcb_alloc_color);
+PHP_FUNCTION(xcb_alloc_named_color);
+PHP_FUNCTION(xcb_create_gc);
+PHP_FUNCTION(xcb_poly_fill_rectangle);
+
+//event_xcb_destroy_notify(xcb_generic_event_t, zval);
+//event_xcb_button_release(xcb_generic_event_t, zval);
+//event_xcb_button_press(xcb_generic_event_t, zval);
+/* 
+  	Declare any global variables you may need between the BEGIN
+	and END macros here:     
+
+ZEND_BEGIN_MODULE_GLOBALS(xcb)
+	long  global_value;
+	char *global_string;
+ZEND_END_MODULE_GLOBALS(xcb)
+*/
+
+/* In every utility function you add that needs to use variables 
+   in php_xcb_globals, call TSRMLS_FETCH(); after declaring other 
+   variables used by that function, or better yet, pass in TSRMLS_CC
+   after the last function argument and declare your utility function
+   with TSRMLS_DC after the last declared argument.  Always refer to
+   the globals in your function as XCB_G(variable).  You are 
+   encouraged to rename these macros something shorter, see
+   examples in any other php module directory.
+*/
+
+#ifdef ZTS
+#define XCB_G(v) TSRMG(xcb_globals_id, zend_xcb_globals *, v)
+#else
+#define XCB_G(v) (xcb_globals.v)
+#endif
+
+#endif	/* PHP_XCB_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
---
+++ trunk/test.php
@@ -0,0 +1,107 @@
+<?php
+echo "Initializing:". xcb_init()."\n";
+echo "Root Width: ".xcb_root_width_in_pixels(1)."\n";
+
+$tmp = xcb_list_extensions();
+var_export($tmp);
+foreach($tmp as $name){
+	$info = xcb_query_extension($name);
+	echo $name .":".$info['first_event']."\n";
+	//var_export(xcb_query_extension($name));
+}
+//exit();
+
+$newid = xcb_generate_id();
+xcb_create_window($newid, xcb_root_id(), 200, 200, 10, 10, 6);
+xcb_configure_window_events($newid, array(1048576,  524288));
+xcb_map_window($newid);
+xcb_flush();
+sleep(1);
+echo "Setting up Events:\n";
+/**
+ * Events:
+ *  XCB_EVENT_MASK_NO_EVENT = 0,
+    XCB_EVENT_MASK_KEY_PRESS = 1,
+    XCB_EVENT_MASK_KEY_RELEASE = 2,
+    XCB_EVENT_MASK_BUTTON_PRESS = 4,
+    XCB_EVENT_MASK_BUTTON_RELEASE = 8,
+    XCB_EVENT_MASK_ENTER_WINDOW = 16,
+    XCB_EVENT_MASK_LEAVE_WINDOW = 32,
+    XCB_EVENT_MASK_POINTER_MOTION = 64,
+    XCB_EVENT_MASK_POINTER_MOTION_HINT = 128,
+    XCB_EVENT_MASK_BUTTON_1_MOTION = 256,
+    XCB_EVENT_MASK_BUTTON_2_MOTION = 512,
+    XCB_EVENT_MASK_BUTTON_3_MOTION = 1024,
+    XCB_EVENT_MASK_BUTTON_4_MOTION = 2048,
+    XCB_EVENT_MASK_BUTTON_5_MOTION = 4096,
+    XCB_EVENT_MASK_BUTTON_MOTION = 8192,
+    XCB_EVENT_MASK_KEYMAP_STATE = 16384,
+    XCB_EVENT_MASK_EXPOSURE = 32768,
+    XCB_EVENT_MASK_VISIBILITY_CHANGE = 65536,
+    XCB_EVENT_MASK_STRUCTURE_NOTIFY = 131072,
+    XCB_EVENT_MASK_RESIZE_REDIRECT = 262144,
+    XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY = 524288,
+    XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT = 1048576,
+    XCB_EVENT_MASK_FOCUS_CHANGE = 2097152,
+    XCB_EVENT_MASK_PROPERTY_CHANGE = 4194304,
+    XCB_EVENT_MASK_COLOR_MAP_CHANGE = 8388608,
+    XCB_EVENT_MASK_OWNER_GRAB_BUTTON = 16777216
+ */
+
+//xcb_configure_window_events($newid, array(1048576, 131072, 524288));
+//xcb_configure_window_events_root($newid);
+xcb_flush();
+echo "Lowering\n";
+//xcb_configure_window_lower($newid);
+//xcb_flush();
+//sleep(1);
+//echo "Raising\n";
+//xcb_configure_window_raise($newid);
+//xcb_flush();
+//xcb_configure_window_size($newid, 300, 300);
+//xcb_flush();
+//sleep(1);
+//var_export(xcb_get_geometry($newid));
+echo "Test Configure: \n";
+//typedef enum xcb_config_window_t {
+//    XCB_CONFIG_WINDOW_X = 1,
+//    XCB_CONFIG_WINDOW_Y = 2,
+//    XCB_CONFIG_WINDOW_WIDTH = 4,
+//    XCB_CONFIG_WINDOW_HEIGHT = 8,
+//    XCB_CONFIG_WINDOW_BORDER_WIDTH = 16,
+//    XCB_CONFIG_WINDOW_SIBLING = 32,
+//    XCB_CONFIG_WINDOW_STACK_MODE = 64
+//} xcb_config_window_t;
+xcb_configure_window($newid, array(1, 2, 4, 8), array(150, 150, 300, 300));
+xcb_flush();
+sleep(1);
+var_export(xcb_get_geometry($newid));
+
+sleep(1);
+//echo "testing by name\n";
+//xcb_configure_window($newid, array("XCB_CONFIG_WINDOW_X", "XCB_CONFIG_WINDOW_Y", "XCB_CONFIG_WINDOW_Z"), array(300, 400));
+//xcb_flush();
+//sleep(4);
+
+echo "Graphics Test:\n";
+
+$intColorMap = xcb_get_default_colormap();
+//xcb_create_colormap($intColorMap, $newid);
+echo "Colormap: {$intColorMap}\n";
+//$red = xcb_alloc_color($intColorMap, "red", 255, 0, 0);
+$red = xcb_alloc_named_color($intColorMap, "Red");
+echo "Red: {$red}\n";
+
+$gcid = xcb_generate_id();
+echo "GC ID: {$gcid}\n";
+xcb_create_gc($newid, $gcid, array(4, 8), array($red, $red));
+
+xcb_poly_fill_rectangle($newid, $gcid, 10, 10, 20, 20);
+
+
+
+echo "End Test\n";
+while ($e = xcb_wait_for_event()){
+	var_export($e);
+}
+?>
