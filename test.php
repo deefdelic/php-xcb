9dcaee04e1815025aa626de517cf530fa3513546
dthompso99 <dthompso99@gmail.com>
2011-03-05 20:22:27 +0000

    made xcb_init() return a resource handle changed ever function to require said resource handle added doc args

--- test.php
+++ test.php
@@ -1,21 +1,25 @@
 <?php
-echo "Initializing:". xcb_init()."\n";
-echo "Root Width: ".xcb_root_width_in_pixels(1)."\n";
+$xcb = xcb_init();
+echo "Initializing:".$xcb ."\n";
+echo "Root Width: ".xcb_root_width_in_pixels($xcb)."\n";
 
-$tmp = xcb_list_extensions();
+$tmp = xcb_list_extensions($xcb);
 var_export($tmp);
+
+
 foreach($tmp as $name){
-	$info = xcb_query_extension($name);
-	echo $name .":".$info['first_event']."\n";
+	//$info = xcb_query_extension($xcb, $name);
+	//echo $name .":".$info['first_event']."\n";
 	//var_export(xcb_query_extension($name));
 }
+
 //exit();
 
-$newid = xcb_generate_id();
-xcb_create_window($newid, xcb_root_id(), 200, 200, 10, 10, 6);
-xcb_configure_window_events($newid, array(1048576,  524288));
-xcb_map_window($newid);
-xcb_flush();
+$newid = xcb_generate_id($xcb);
+xcb_create_window($xcb, $newid, xcb_root_id($xcb), 200, 200, 10, 10, 6);
+xcb_configure_window_events($xcb, $newid, array(1048576,  524288));
+xcb_map_window($xcb, $newid);
+xcb_flush($xcb);
 sleep(1);
 echo "Setting up Events:\n";
 /**
@@ -50,7 +54,7 @@ echo "Setting up Events:\n";
 
 //xcb_configure_window_events($newid, array(1048576, 131072, 524288));
 //xcb_configure_window_events_root($newid);
-xcb_flush();
+xcb_flush($xcb);
 echo "Lowering\n";
 //xcb_configure_window_lower($newid);
 //xcb_flush();
@@ -72,10 +76,10 @@ echo "Test Configure: \n";
 //    XCB_CONFIG_WINDOW_SIBLING = 32,
 //    XCB_CONFIG_WINDOW_STACK_MODE = 64
 //} xcb_config_window_t;
-xcb_configure_window($newid, array(1, 2, 4, 8), array(150, 150, 300, 300));
-xcb_flush();
+xcb_configure_window($xcb, $newid, array(1, 2, 4, 8), array(150, 150, 300, 300));
+xcb_flush($xcb);
 sleep(1);
-var_export(xcb_get_geometry($newid));
+var_export(xcb_get_geometry($xcb, $newid));
 
 sleep(1);
 //echo "testing by name\n";
@@ -85,23 +89,23 @@ sleep(1);
 
 echo "Graphics Test:\n";
 
-$intColorMap = xcb_get_default_colormap();
+$intColorMap = xcb_get_default_colormap($xcb);
 //xcb_create_colormap($intColorMap, $newid);
 echo "Colormap: {$intColorMap}\n";
 //$red = xcb_alloc_color($intColorMap, "red", 255, 0, 0);
-$red = xcb_alloc_named_color($intColorMap, "Red");
+$red = xcb_alloc_named_color($xcb, $intColorMap, "Red");
 echo "Red: {$red}\n";
 
-$gcid = xcb_generate_id();
+$gcid = xcb_generate_id($xcb);
 echo "GC ID: {$gcid}\n";
-xcb_create_gc($newid, $gcid, array(4, 8), array($red, $red));
+xcb_create_gc($xcb, $newid, $gcid, array(4, 8), array($red, $red));
 
-xcb_poly_fill_rectangle($newid, $gcid, 10, 10, 20, 20);
-
+xcb_poly_fill_rectangle($xcb, $newid, $gcid, 10, 10, 20, 20);
+xcb_flush($xcb);
 
 
 echo "End Test\n";
-while ($e = xcb_wait_for_event()){
+while ($e = xcb_wait_for_event($xcb)){
 	var_export($e);
 }
 ?>
